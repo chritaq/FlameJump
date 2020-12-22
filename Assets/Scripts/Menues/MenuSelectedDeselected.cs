5ca17901f0a3bb8cff4964e2369911c13cc35840
Christopher <chrtaq1027@skola.goteborg.se>
2020-12-22 15:39:42 +0100

    Updated menu

---
+++ Assets/Scripts/Menues/MenuSelectedDeselected.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuSelectedDeselected : MonoBehaviour, IDeselectHandler, ISelectHandler
+{
+    [SerializeField] private GameObject background;
+    public void OnDeselect(BaseEventData eventData)
+    {
+        background.SetActive(false);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        background.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        background.SetActive(false);
+    }
+}

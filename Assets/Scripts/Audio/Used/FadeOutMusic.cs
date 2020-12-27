931ae82b944cb9f8e73688b83a8f087e2cc27a98
Christopher <chrtaq1027@skola.goteborg.se>
2020-12-27 18:44:19 +0100

    Fixed bug with fadeout between scenes

--- Assets/Scripts/Audio/Used/FadeOutMusic.cs
+++ Assets/Scripts/Audio/Used/FadeOutMusic.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class FadeOutMusic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        DontDestroyOnLoad(this);
     }
 
     public void StartFadeOut(AudioSource[] audioSources)
@@ -15,24 +14,24 @@ public class FadeOutMusic : MonoBehaviour
         for(int i = 0; i < audioSources.Length; i++)
         {
             StartCoroutine(FadeOut(audioSources[i]));
+            //audioSources[i].Stop();
         }
-
+        //Destroy(this, 5);
     }
 
-    private float fadeRate = 0.5f;
+    private float fadeOutStep = 0.01f;
     private IEnumerator FadeOut(AudioSource audioSource)
     {
         float startVolume = audioSource.volume;
 
         while (audioSource.volume > 0)
         {
-            audioSource.volume -= Time.deltaTime * fadeRate;
-            yield return new WaitForEndOfFrame();
+            audioSource.volume -= fadeOutStep;
+            yield return new WaitForSecondsRealtime(0.01f);
         }
 
         audioSource.Stop();
         audioSource.volume = startVolume;
-        Destroy(this, 0.1f);
         yield return null;
     }
 }
